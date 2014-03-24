338045bbf9ad22e05de8ba6f817eb1b6f1ee2e51
James Bayer <jbayer@gopivotal.com>
2014-03-24 10:04:50 -0700

    fix run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Used by buildpack https://github.com/shageman/buildpack-binary
-
 chmod +x ./server
+exec ./server -listen-address ":${PORT}"
 
-exec ./server                                                           \
-	-listen-address            ":${PORT}"                                 \
-	-http-discoverer-endpoint  "http://peer-example.cfapps.io/_discover"  \
-	-http-discoverer-every     5s
